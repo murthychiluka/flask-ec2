4c802b543dc6429e2e86ed80931e82eb8aea7af6
murthychiluka <murthychiluka@gmail.com>
2026-06-30 23:48:04 +0530

    added murthy

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ def hello():
     return "updated Flask sample application on azure hghapp service updated verrsion-4"
 
 
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True,host='0.0.0.0',port=port)
+    murthy
